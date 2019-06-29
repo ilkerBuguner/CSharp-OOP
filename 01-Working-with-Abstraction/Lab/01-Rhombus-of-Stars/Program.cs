3674107671772fed6865f361ff906374a70d0716
İlker Bugüner <ilker.buguner@gmail.com>
2019-06-29 14:36:17 +0300

    Solved some lab problems from Working with Abstraction lecture.

---
+++ 01-Working-with-Abstraction/Lab/01-Rhombus-of-Stars/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab___1._Rhombus_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= size; i++)
+            {
+                PrintRow(i, size);
+            }
+            for (int i = size - 1; i >= 1; i--)
+            {
+                PrintRow(i, size);
+            }
+        }
+
+        private static void PrintRow(int i, int size)
+        {
+            int spacesCount = size - i;
+            for (int j = 0; j < spacesCount; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k < i; k++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine("*");
+        }
+    }
+}
---
+++ 01-Working-with-Abstraction/Lab/02-Point-in-Rectangle/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        int[] pointsArgs = ArrayReader();
+
+        Point topLeft = new Point(pointsArgs[0], pointsArgs[1]);
+        Point bottomRight = new Point(pointsArgs[2], pointsArgs[3]);
+        Rectangle rectangle = new Rectangle(topLeft, bottomRight);
+
+        int timesToRead = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < timesToRead; i++)
+        {
+            int[] currentPointInfo = ArrayReader();
+            Point currentPoint = new Point(currentPointInfo[0], currentPointInfo[1]);
+
+            Console.WriteLine(rectangle.Contains(currentPoint));
+        }
+    }
+
+    public static int[] ArrayReader()
+    {
+        return Console.ReadLine().Split().Select(int.Parse).ToArray();
+    }
+}
+
