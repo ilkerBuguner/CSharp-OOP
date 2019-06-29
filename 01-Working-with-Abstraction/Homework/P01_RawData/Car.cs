ebfc5aae183b09b3b1dc5afcda7738cf4a973cd8
İlker Bugüner <ilker.buguner@gmail.com>
2019-06-30 01:57:36 +0300

    Solved some problems from Working with Abstraction homework.

---
+++ 01-Working-with-Abstraction/Homework/P01_RawData/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_RawData
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tire[] Tires { get; set; }
+
+        public Car(string model, int engineSpeed, int enginePower,
+            int cargoWeight, string cargoType, double tire1Pressure,
+            int tire1Age, double tire2Pressure, int tire2Age,
+            double tire3Pressure, int tire3age, double tire4Pressure, int tire4age)
+        {
+            Model = model;
+            Engine = new Engine(engineSpeed, enginePower);
+            Cargo = new Cargo(cargoWeight, cargoType);
+            Tires = new Tire[4];
+
+            Tires[0] = new Tire(tire1Pressure, tire1Age);
+            Tires[1] = new Tire(tire2Pressure, tire2Age);
+            Tires[2] = new Tire(tire3Pressure, tire3age);
+            Tires[3] = new Tire(tire4Pressure, tire4age);
+        }
+    }
+}
---
+++ 01-Working-with-Abstraction/Homework/P03_JediGalaxy/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_JediGalaxy
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] dimestions = ReadArray();
+
+            int x = dimestions[0];
+            int y = dimestions[1];
+
+            int[,] matrix = new int[x, y];
+
+            int value = 0;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    matrix[i, j] = value++;
+                }
+            }
+
+            string command = Console.ReadLine();
+            long sum = 0;
+            while (command != "Let the Force be with you")
+            {
+                int[] ivoS = command
+                    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int[] evil = ReadArray();
+                int xE = evil[0];
+                int yE = evil[1];
+                EvilPathMaker(matrix, ref xE, ref yE);
+
+                int xI = ivoS[0];
+                int yI = ivoS[1];
+                IvoPathMaker(matrix, ref sum, ref xI, ref yI);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(sum);
+
+        }
+
+        private static void IvoPathMaker(int[,] matrix, ref long sum, ref int xI, ref int yI)
+        {
+            while (xI >= 0 && yI < matrix.GetLength(1))
+            {
+                if (xI >= 0 && xI < matrix.GetLength(0) && yI >= 0 && yI < matrix.GetLength(1))
+                {
+                    sum += matrix[xI, yI];
+                }
+
+                yI++;
+                xI--;
+            }
+        }
+
+        private static void EvilPathMaker(int[,] matrix, ref int xE, ref int yE)
+        {
+            while (xE >= 0 && yE >= 0)
+            {
+                if (xE >= 0 && xE < matrix.GetLength(0) && yE >= 0 && yE < matrix.GetLength(1))
+                {
+                    matrix[xE, yE] = 0;
+                }
+                xE--;
+                yE--;
+            }
+        }
+
+        private static int[] ReadArray()
+        {
+            return Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
