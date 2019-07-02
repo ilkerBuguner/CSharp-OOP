0b092f8db592fdc0ec5de8b4272b56d114959625
İlker Bugüner <ilker.buguner@gmail.com>
2019-07-03 02:13:43 +0300

    Solved all lab problems from Inheritance lecture.

---
+++ 02-Inheritance/Lab/04-Random-List/RandomList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+
+        public RandomList()
+        {
+            rnd = new Random();
+        }
+
+        public string RandomString()
+        {
+            int index = rnd.Next(this.Count);
+            string textToRemove = this[index];
+            this.RemoveAt(index);
+            return textToRemove;
+        }
+
+
+    }
+}
---
+++ 02-Inheritance/Lab/04-Random-List/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+            for (int i = 0; i < 100; i++)
+            {
+                randomList.Add(i.ToString());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(randomList.RandomString());
+            }
+        }
+    }
+}
