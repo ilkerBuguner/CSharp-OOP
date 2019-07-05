8943b68a42f5262ef4f98c4ad45f41e4051a6217
İlker Bugüner <ilker.buguner@gmail.com>
2019-07-05 14:49:20 +0300

    Solved homework problems from Inheritance lecture.

---
+++ 02-Inheritance/Homework/04-NeedForSpeed/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SportCar car = new SportCar(300, 20);
+
+            System.Console.WriteLine(car.FuelConsumption);
+            car.Drive(2);
+
+        }
+    }
+}
---
+++ 02-Inheritance/Homework/04-NeedForSpeed/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double defaultFuelConsumption = 1.25;
+        public virtual double FuelConsumption => defaultFuelConsumption;
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+        public virtual void Drive(double kilometers)
+        {
+            double fuelNeeded = FuelConsumption * kilometers;
+            if (Fuel >= fuelNeeded)
+            {
+                Fuel -= FuelConsumption * kilometers;
+
+            }
+        }
+        public Vehicle(int horsePower, double fuel)
+        {
+            Fuel = fuel;
+            HorsePower = horsePower;
+        }
+
+
+    }
+}
