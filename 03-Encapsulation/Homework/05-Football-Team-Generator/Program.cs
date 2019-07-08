1435357e29bdde42e928008cef2fc6f5e05c825a
İlker Bugüner <ilker.buguner@gmail.com>
2019-07-09 00:07:35 +0300

    Solved ALL homework problems from Encapsulation lecture.

---
+++ 03-Encapsulation/Homework/05-Football-Team-Generator/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Football_Team_Generator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, Team> teams = new Dictionary<string, Team>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    if (tokens[0] == "Team")
+                    {
+                        string teamName = tokens[1];
+                        Team currentTeam = new Team(teamName);
+                        teams.Add(teamName, currentTeam);
+                    }
+                    else if (tokens[0] == "Add")
+                    {
+                        string teamNameToAddInto = tokens[1];
+                        if (teams.ContainsKey(teamNameToAddInto))
+                        {
+                            string playerName = tokens[2];
+                            int endurance = int.Parse(tokens[3]);
+                            int sprint = int.Parse(tokens[4]);
+                            int dribble = int.Parse(tokens[5]);
+                            int passing = int.Parse(tokens[6]);
+                            int shooting = int.Parse(tokens[7]);
+
+                            Player currentPlater = new Player(playerName, endurance, sprint,
+                                dribble, passing, shooting);
+
+                            teams[teamNameToAddInto].AddPlayer(currentPlater);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Team {teamNameToAddInto} does not exist.");
+                        }
+                    }
+                    else if (tokens[0] == "Remove")
+                    {
+                        string teamName = tokens[1];
+                        string playerName = tokens[2];
+                        if (teams.ContainsKey(teamName))
+                        {
+                            teams[teamName].RemovePlayer(playerName);
+                        }
+                    }
+                    else if (tokens[0] == "Rating")
+                    {
+                        string teamName = tokens[1];
+                        if (teams.ContainsKey(teamName))
+                        {
+                            teams[teamName].CalculateRating();
+                            Console.WriteLine($"{teamName} - {teams[teamName].Rating}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Team {teamName} does not exist.");
+                        }
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+        }
+    }
+}
---
+++ 03-Encapsulation/Homework/05-Football-Team-Generator/Team.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05._Football_Team_Generator
+{
+    public class Team
+    {
+        private string name;
+
+        private List<Player> players;
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                name = value;
+            }
+        }
+
+        public Team(string name)
+        {
+            Name = name;
+            players = new List<Player>();
+        }
+        public double Rating { get; private set; }
+
+        public void CalculateRating()
+        {
+            double totalPlayersOverallSkill = 0;
+            if (players.Count > 0)
+            {
+                Rating = (int)Math.Round(players.Average(x => x.Stats));
+            }
+            else
+            {
+                Rating = (int)totalPlayersOverallSkill;
+            }
+
+        }
+
+        public void RemovePlayer(string name)
+        {
+            Player player = players.FirstOrDefault(x => x.Name == name);
+
+            if (player != null)
+            {
+                players.Remove(player);
+            }
+            else
+            {
+                Console.WriteLine($"Player {name} is not in {Name} team.");
+            }
+        }
+
+        public void AddPlayer(Player player)
+        {
+            players.Add(player);
+        }
+    }
+}
