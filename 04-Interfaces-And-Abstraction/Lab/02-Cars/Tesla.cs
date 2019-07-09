40ae133bffc177c5270f2ad3d3d5b2710c949c49
İlker Bugüner <ilker.buguner@gmail.com>
2019-07-10 02:38:11 +0300

    Solved all problems from Interfaces And Abstraction lecture.

---
+++ 04-Interfaces-And-Abstraction/Lab/02-Cars/Tesla.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Batery { get; set; }
+
+
+        public Tesla(string model, string color, int batery)
+        {
+            Model = model;
+            Color = color;
+            Batery = batery;
+        }
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{Color} Tesla {Model} with {Batery} Batteries");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
