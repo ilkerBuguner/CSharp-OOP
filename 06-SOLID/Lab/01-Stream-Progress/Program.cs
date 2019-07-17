efca5d54e1889432fed08a2ce12a79038a0453d8
İlker Bugüner <ilker.buguner@gmail.com>
2019-07-18 02:47:25 +0300

    Solved lab problems from SOLID lecture.

---
+++ 06-SOLID/Lab/01-Stream-Progress/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Music(string artist, string album, int length, int bytesSent)
+
+            Music music = new Music("Sancak", "Yagmur", 4, 133);
+
+            StreamProgressInfo streamProgress = new StreamProgressInfo(music);
+
+
+        }
+    }
+}
