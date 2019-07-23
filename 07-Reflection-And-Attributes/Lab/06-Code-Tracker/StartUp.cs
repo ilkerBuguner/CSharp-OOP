3594259206319d8b02aac967f3976b70d5ba8b15
İlker Bugüner <ilker.buguner@gmail.com>
2019-07-23 15:51:03 +0300

    Solved Reflection And Attributes lecture lab.

---
+++ 07-Reflection-And-Attributes/Lab/06-Code-Tracker/StartUp.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+[Author("Ventsi")]
+public class StartUp
+{
+    [Author("Gosho")]
+    public static void Main(string[] args)
+    {
+        Tracker tracker = new Tracker();
+        Tracker.PrintMethodsByAuthor();
+    }
+}
+
+
---
+++ 07-Reflection-And-Attributes/Lab/06-Code-Tracker/Tracker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+
+public class Tracker
+{
+    public static void PrintMethodsByAuthor()
+    {
+        Type classType = typeof(StartUp);
+
+        MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance 
+            | BindingFlags.Public | BindingFlags.Static);
+
+        foreach (var method in classMethods)
+        {
+            if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+            {
+                var attributes = method.GetCustomAttributes(false);
+                foreach (AuthorAttribute attribute in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                }
+            }
+        }
+    }
+}
+
