506d7ffa00ed92c1b917e9d010fcfdcdba5e72ab
İlker Bugüner <ilker.buguner@gmail.com>
2019-07-25 23:42:44 +0300

    Solved Reflection And Attributes homework.

---
+++ 07-Reflection-And-Attributes/Homework/02-Validation-Attributes/Entities/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Entities
+{
+    public class Person
+    {
+        private const int minAge = 12;
+        private const int maxAge = 90;
+
+        [MyRequired]
+        public string FullName { get; private set; }
+
+        [MyRange(minAge, maxAge)]
+        public int Age { get; private set; }
+
+        public Person(string fullname, int age)
+        {
+            FullName = fullname;
+            Age = age;
+        }
+    }
+}
---
+++ 07-Reflection-And-Attributes/Homework/02-Validation-Attributes/Utilities/Validator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Utilities
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] propertyInfos = obj.GetType().GetProperties();
+
+            foreach (var property in propertyInfos)
+            {
+                MyValiditionAttribute[] attributes = property
+                    .GetCustomAttributes()
+                    .Where(a => a is MyValiditionAttribute)
+                    .Cast<MyValiditionAttribute>()
+                    .ToArray();
+
+                foreach (MyValiditionAttribute attribute in attributes)
+                {
+                    if (!attribute.IsValid(property.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
