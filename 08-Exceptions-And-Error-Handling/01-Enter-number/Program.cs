82d26c245243af6d1b2162ddf691d036c345c995
İlker Bugüner <ilker.buguner@gmail.com>
2019-07-28 13:48:20 +0300

    Solved some problems from Exceptions And Error Handling lecture.

---
+++ 08-Exceptions-And-Error-Handling/01-Enter-number/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Enter_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string input = Console.ReadLine();
+
+                try
+                {
+                    ReadNumber(input);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);;
+                    i = 0;
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine(fe.Message);
+                    i = 0;
+                }
+            }
+        }
+
+        public static void ReadNumber(string number)
+        {
+            int num = 0;
+
+            int.TryParse(number, out num);
+
+            if (num != 0)
+            {
+                if (num < 1 || num > 100)
+                {
+                    throw new ArgumentException("Invalid number");
+                }
+            }
+            else if (num == 0)
+            {
+                throw new FormatException("Invalid input");
+            }
+
+            Console.WriteLine(num);
+
+        }
+    }
+}
---
+++ 08-Exceptions-And-Error-Handling/02-Square-root/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exceptions_And_Error_Handling___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            try
+            {
+                double num = GetSquareRoot(number);
+                Console.WriteLine(num);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+
+        private static double GetSquareRoot(double number)
+        {
+            if (number == null || number < 0)
+            {
+                throw new InvalidOperationException("Invalid Number");
+            }
+
+            return Math.Sqrt(number);
+        }
+    }
+}
