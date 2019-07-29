4580cc0206f2a8935eaf70664d2e005f6a0ec277
İlker Bugüner <ilker.buguner@gmail.com>
2019-07-29 19:34:40 +0300

    Solved some problems from Exceptions And Error Handling lecture.

---
+++ 08-Exceptions-And-Error-Handling/03-Valid-Person/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Valid_Person
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstname, string lastname, int age)
+        {
+            FirstName = firstName;
+            Lastname = lastName;
+            Age = age;
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Invalid Firstname!");
+                }
+
+                firstName = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get => lastName;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Invalid Lastname!");
+                }
+
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 0 || value > 120)
+                {
+                    throw new ArgumentNullException("Invalid age!");
+                }
+
+                age = value;
+            }
+        }
+    }
+}
---
+++ 08-Exceptions-And-Error-Handling/03-Valid-Person/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Valid_Person
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
